fdf8d673cdfb99aca41e545d96ead9340abac77e
Vicente-AR81 <vicentearhex@gmail.com>
2025-09-09 14:38:05 -0300

    css de formulario de incidentes

--- app/app.py
+++ app/app.py
@@ -10,7 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Usuario en base de datos
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
